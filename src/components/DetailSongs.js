3f14b6a5b1ad96cbaa2dd370b8efdeeda864c58d
Phương Hải Đăng <93462885+haidaqn@users.noreply.github.com>
2022-11-11 01:36:52 +0700

    commit-1

--- src/components/DetailSongs.js
+++ src/components/DetailSongs.js
@@ -10,11 +10,11 @@ export default function DetaiLSongs() {
     <div className='col-span-1 p-3'>
       <h2 className='text-cyan-500 font-bold'>Now playing</h2>
       <h2 className= 'text-zinc-400  md:text-2xl text-base mb-5' >{ song.name }</h2>
-      <div className='md:w-[300px] w-[100px] md:m-auto md:mt-10'>
-        <img className='w-full object-cover' alt='avatar' src={song.links.images[0].url} />
+      <div className='md:w-[230px] lg:w-[400px] w-[100px] md:m-auto md:mt-10'>
+        <img className='w-full object-cover rounded-lg	' alt='avatar' src={song.links.images[0].url} />
       </div>
       <div className='md:flex md:justify-evenly items-center mt-5' >
-        <img className='md:rounded-full md:w-[70px] w-0' alt='avatar' src={song.links.images[1].url} />
+        <img className='md:rounded-full md:w-[100px] w-0' alt='avatar' src={song.links.images[1].url} />
         <span className='md:text-2xl text-white'>{song.author }</span>
       </div>
     </div>
